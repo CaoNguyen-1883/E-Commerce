7ef3fbd2b89ae26b6f263546edc9256eeecb3b62
CaoNguyen-1883 <nguyendadenkrb@gmail.com>
2025-10-27 09:18:11 +0700

    fix: mapping user - role - permission

--- ecommerce/src/main/java/dev/CaoNguyen_1883/ecommerce/user/repository/UserRepository.java
+++ ecommerce/src/main/java/dev/CaoNguyen_1883/ecommerce/user/repository/UserRepository.java
@@ -4,6 +4,7 @@ import dev.CaoNguyen_1883.ecommerce.user.entity.Role;
 import dev.CaoNguyen_1883.ecommerce.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,8 +15,11 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-
+    @EntityGraph(attributePaths = {"roles", "roles.permissions"})
     Optional<User> findByEmail(String email);
+
+    @EntityGraph(attributePaths = {"roles", "roles.permissions"})
+    Optional<User> findById(UUID id);
 
     boolean existsByEmail(String email);
 
